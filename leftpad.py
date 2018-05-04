1f53a27c55e750dc1ae16a1c5e85002e30eb376b
404 <34942042+neko404notfound@users.noreply.github.com>
2018-05-04 13:09:20 +0100

    Create leftpad.py

---
+++ leftpad.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Left-pads a string, obviously.
+"""
+
+__all__ = ('leftpad',)
+
+
+def leftpad(string: str, target_length: int) -> str:
+    """
+    Left-pads the given string until it is the given 
+    length. If you actually use this as a dependency,
+    you are a meme, and should return to the cabbage
+    farm.
+    :param string: the string to left pad.
+    :param target_length: the length of the result
+    :returns: the padded string. If len(string) >=
+        target_length, nothing is done.
+    """
+    return max(0, target_length - len(string)) * ' ' + string
